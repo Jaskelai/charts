3e480101bc1624c8d04d6a4a2ad4ab4846522985
KornilovMikhail <kornilovmisha1999@gmail.com>
2019-10-06 16:38:42 +0300

    updated module name

--- chartview/src/main/java/com/github/jaskelai/chartcustomview/MainActivity.kt
+++ chartview/src/main/java/com/github/jaskelai/chartcustomview/MainActivity.kt
@@ -2,11 +2,15 @@ package com.github.jaskelai.chartcustomview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        chartview_main.values = mapOf("A" to 10, "B" to 15, "C" to 12)
+        chartview_main.chartMargins = 5
     }
 }
